de67f1c5f2e2cdba20403811e75e8f35c2e8f6d4
GalaxyEater-NA <minhtuanhoang310701@gmail.com>
2026-05-13 11:09:21 -0500

    Add sandbox2.js and include additional JavaScript examples

---
+++ sandbox2.js
@@ -0,0 +1,21 @@
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+// ninjas[1] = 'ken';
+// console.log(ninjas[1])
+
+// let ages = [20,25, 30, 35];
+// console.log(ages[2]);
+
+// let random = ['shawn', 'crystal', 30, 20];
+// console.log(random);
+
+// console.log(ninjas.length);
+
+// array methods
+
+// let som = ninjas.join(',');
+// let som = ninjas.indexOf('chun-li');
+// let som = ninjas.concat(['ken', 'crystal']);
+let som = ninjas.push('ken');
+
+console.log(som);
