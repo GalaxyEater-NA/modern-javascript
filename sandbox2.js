1f57f47dda3b5e13a5b3a5bca08eaa6206ee1d18
GalaxyEater-NA <minhtuanhoang310701@gmail.com>
2026-05-13 14:04:14 -0500

    Refactor sandbox2.js to include additional console logs for boolean and comparison operators

--- sandbox2.js
+++ sandbox2.js
@@ -17,5 +17,41 @@ let ninjas = ['shaun', 'ryu', 'chun-li'];
 // let som = ninjas.indexOf('chun-li');
 // let som = ninjas.concat(['ken', 'crystal']);
 let som = ninjas.push('ken');
+som = ninjas.pop();
 
-console.log(som);
+console.log(ninjas, som);
+
+let age = null;
+
+console.log(age, age + 3, `the age is ${age}`);
+
+// boolean & comparison operators
+console.log(true, false, 'true', 'false');
+
+// methods can return booleans
+let email1 = 'john@example.com';
+let names = ['mario', 'luigi', 'toad'];
+
+// let emailResult = email.includes('@');
+// let namesResult = names.includes('luigi');
+// console.log(emailResult);
+// console.log(namesResult);
+
+// comparison operators
+let age1 = 24;
+
+// console.log(age1 == 25);
+// console.log(age1 == 24);
+// console.log(age1 != 25);
+// console.log(age1 < 25);
+// console.log(age1 > 25);
+// console.log(age1 <= 25);
+// console.log(age1 >= 25);
+
+let name = 'shaun';
+console.log(name == 'shaun');
+console.log(name == 'Shaun');
+console.log(name > 'crystal');
+console.log(name > 'Shaun');
+console.log(name > 'Crystal');
+
